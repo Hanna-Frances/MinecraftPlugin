6dd8c16dcca46a91d570c41e2ee0fcbc0881a59c
Someoneawesome78 <Someoneawesome78@github.com>
2019-09-13 21:52:41 -0400

    Added executor

--- src/gate/Main.java
+++ src/gate/Main.java
@@ -4,10 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 
 public class Main extends JavaPlugin{
+	
+	private commandManager cmdManager;
 
     @Override
     public void onEnable(){
         Bukkit.getServer().getConsoleSender().sendMessage("persona");
+        
+        cmdManager = new commandManager();
+        
+        getCommand("gate").setExecutor(cmdManager);
+        getCommand("gate").setTabCompleter(cmdManager);
+        
     }
     
     
